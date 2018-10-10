c2f49fd860f14c817cdf3f4573ae1768f37fc15d
jordan <jsadi@my.wctc.edu>
2018-10-10 18:25:01 -0500

    JavaDoc corrected

--- src/edu/wctc/jsadi/FileOutput.java
+++ src/edu/wctc/jsadi/FileOutput.java
@@ -16,7 +16,6 @@ public class FileOutput {
     /**
      * This constructor creates an instance of the FileOutput class
      * @param fileName the name of the specified file
-     * @return instance of the FileOutput class
      */
     public FileOutput(String fileName) {
         this.fileName = fileName;
@@ -31,7 +30,6 @@ public class FileOutput {
     /**
      * This method attempts to write a line to the specified file
      * @param line the line that is meant to be written to the file
-     * @throws Exception if the file cannot be written to
      */
     public void fileWrite(String line) {
         try {
@@ -44,7 +42,6 @@ public class FileOutput {
 
     /**
      * This method attempts to close the specified file
-     * @throws IOException if the file cannot be closed
      */
     public void fileClose() {
         if (out != null) {
